fdbbc9c02434d8c086c934e9226e643cd30458fb
kolyan <vkont4@gmail.com>
2018-10-19 14:00:00 +0200

    Init

---
+++ src/main/java/io/github/yashchenkon/softdelete/SpringBootSoftDeleteStarterApplication.java
@@ -0,0 +1,12 @@
+package io.github.yashchenkon.softdelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoftDeleteStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSoftDeleteStarterApplication.class, args);
+    }
+}
