42842ad879cc2409dd7448f8ef934c06b35dbaf9
kolyan <vkont4@gmail.com>
2018-10-22 14:00:00 +0200

    Add repository interface

--- src/main/java/io/github/yashchenkon/softdelete/repository/SoftDeleteRepository.java
+++ src/main/java/io/github/yashchenkon/softdelete/repository/SoftDeleteRepository.java
@@ -1,7 +1,9 @@
 package io.github.yashchenkon.softdelete.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @author Mykola Yashchenko
  */
-public interface SoftDeleteRepository {
+public interface SoftDeleteRepository<T, ID> extends JpaRepository<T, ID> {
 }
