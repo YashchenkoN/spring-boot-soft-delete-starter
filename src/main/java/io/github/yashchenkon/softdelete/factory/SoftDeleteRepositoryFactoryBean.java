bfe7b7b8433f9f6c73f94b6e08acc9005459e04d
kolyan <vkont4@gmail.com>
2018-10-31 10:36:00 +0200

    Use BaseEntity bounds for generics

--- src/main/java/io/github/yashchenkon/softdelete/factory/SoftDeleteRepositoryFactoryBean.java
+++ src/main/java/io/github/yashchenkon/softdelete/factory/SoftDeleteRepositoryFactoryBean.java
@@ -5,12 +5,14 @@ import javax.persistence.EntityManager;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 
+import io.github.yashchenkon.softdelete.entity.BaseEntity;
 import io.github.yashchenkon.softdelete.repository.SoftDeleteRepository;
 
 /**
  * @author Mykola Yashchenko
  */
-public class SoftDeleteRepositoryFactoryBean<T extends SoftDeleteRepository<S, ID>, S, ID> extends JpaRepositoryFactoryBean<T, S, ID> {
+public class SoftDeleteRepositoryFactoryBean<T extends SoftDeleteRepository<S, ID>, S extends BaseEntity, ID>
+        extends JpaRepositoryFactoryBean<T, S, ID> {
 
     public SoftDeleteRepositoryFactoryBean(final Class<? extends T> repositoryInterface) {
         super(repositoryInterface);
--- src/main/java/io/github/yashchenkon/softdelete/repository/SoftDeleteRepository.java
+++ src/main/java/io/github/yashchenkon/softdelete/repository/SoftDeleteRepository.java
@@ -2,8 +2,10 @@ package io.github.yashchenkon.softdelete.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import io.github.yashchenkon.softdelete.entity.BaseEntity;
+
 /**
  * @author Mykola Yashchenko
  */
-public interface SoftDeleteRepository<T, ID> extends JpaRepository<T, ID> {
+public interface SoftDeleteRepository<T extends BaseEntity, ID> extends JpaRepository<T, ID> {
 }
